c6f2839108a3b898965a4aa93d662049a01097cf
Tobias Bester <tobias@labs.epiuse.com>
2025-09-28 10:05:19 +0200

    Initialize Project
    - Create Java 25 project with Maven
    
    Ticket: #AF-001

---
+++ agentic-football-app/src/main/java/org/coolandfunandnice/Main.java
@@ -0,0 +1,17 @@
+package org.coolandfunandnice;
+
+import org.coolandfunandnice.matchengine.Match;
+import org.coolandfunandnice.matchengine.factory.MatchFactory;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        IO.println("Hello and welcome to Agentic Football!");
+
+        Match defaultMatch = MatchFactory.createDefaultMatch(1);
+        defaultMatch.printMatchSetup();
+    }
+}
